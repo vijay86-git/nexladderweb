b30d72bb93500ea20a7dd673524a62fb716b6d2b
Vijay Singh <vjmail17@gmail.com>
2021-09-01 11:39:50 +0530

    commit

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
+    header('Location: http'.(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'').'://' . substr($_SERVER['HTTP_HOST'], 4).$_SERVER['REQUEST_URI']);
+    exit;
+}
+
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
