45e27150d9f6d1368af9b7be0f19c674da6e4fe6
Vijay Singh <vjmail17@gmail.com>
2021-09-13 20:38:36 +0530

    ....

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -1,7 +1,16 @@
 <?php
 
-defined('DOC_ROOT_BACKEND_CSS') || define('DOC_ROOT_BACKEND_CSS', '/Users/user/nexladderweb/public/backend/assets/css/');
-defined('DOC_ROOT_BACKEND_JS')  || define('DOC_ROOT_BACKEND_JS', '/Users/user/nexladderweb/public/backend/assets/js/');
+if($_SERVER['SERVER_NAME'] == "dev.nexladder.local")
+ {
+    defined('DOC_ROOT_BACKEND_CSS') || define('DOC_ROOT_BACKEND_CSS', '/Users/user/nexladderweb/public/backend/assets/css/');
+    defined('DOC_ROOT_BACKEND_JS')  || define('DOC_ROOT_BACKEND_JS', '/Users/user/nexladderweb/public/backend/assets/js/');
+ }
+ else
+  {
+        defined('DOC_ROOT_BACKEND_CSS') || define('DOC_ROOT_BACKEND_CSS', '/var/www/html/nexladderweb/public/backend/assets/css/');
+        defined('DOC_ROOT_BACKEND_JS')  || define('DOC_ROOT_BACKEND_JS', '/var/www/html/nexladderweb/public/backend/assets/js/');
+  }
+
 /*
  | --------------------------------------------------------------------
  | App Namespace
