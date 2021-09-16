eecdd2b4f516037a1064f3f47efdac172c3f7a67
Vijay Singh <vjmail17@gmail.com>
2021-09-16 23:05:30 +0530

    ...

--- app/Views/Frontend/Converter/Unit.php
+++ app/Views/Frontend/Converter/Unit.php
@@ -5,6 +5,7 @@
  ul.liststyles{border:1px solid #f1f1f1;margin:0;padding:0;list-style-type:none;margin-bottom:20px}
  ul.liststyles li {line-height:34px;font-size:13px;border-bottom:1px solid #f1f1f1;padding-left:10px;}
  select option {padding:5px 10px;}
+ .activecls{background:#e6e6e6}
 </style>
          <div class="">
             <div class="row">
@@ -12,43 +13,43 @@
 
                	  <ul class="liststyles"> 
 
-               	  	<li id="length-converter" class="currentselected"><span><a href="<?php echo route_to('unit.convertor', 'length-converter') ?>">Length Converter</a> </span> </li> 
+               	  	<li class="<?php echo ($converter == "length-converter") ? "activecls" : "" ?>" id="length-converter" class="currentselected"><span><a href="<?php echo route_to('unit.convertor', 'length-converter') ?>">Length Converter</a> </span> </li> 
 
-               	  	<li id="weight-converter"><span><a href="<?php echo route_to('unit.convertor', 'weight-converter') ?>">Weight Converter</a> </span> </li> 
+               	  	<li class="<?php echo ($converter == "weight-converter") ? "activecls" : "" ?>" id="weight-converter"><span><a href="<?php echo route_to('unit.convertor', 'weight-converter') ?>">Weight Converter</a> </span> </li> 
 
-               	  	<li id="volume-converter"><span><a href="<?php echo route_to('unit.convertor', 'volume-converter') ?>">Volume Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "volume-converter") ? "activecls" : "" ?>" id="volume-converter"><span><a href="<?php echo route_to('unit.convertor', 'volume-converter') ?>">Volume Converter </a> </span></li> 
 
-               	  	<li id="area-converter"><span><a href="<?php echo route_to('unit.convertor', 'area-converter') ?>">Area Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "area-converter") ? "activecls" : "" ?>" id="area-converter"><span><a href="<?php echo route_to('unit.convertor', 'area-converter') ?>">Area Converter </a> </span></li> 
 
-               	  	<li id="temperature-converter"><span><a href="<?php echo route_to('unit.convertor', 'temperature-converter') ?>">Temperature Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "temperature-converter") ? "activecls" : "" ?>" id="temperature-converter"><span><a href="<?php echo route_to('unit.convertor', 'temperature-converter') ?>">Temperature Converter </a> </span></li> 
 
-               	  	<li id="speed-converter"><span><a href="<?php echo route_to('unit.convertor', 'speed-converter') ?>">Speed Converter </a> </span></li>
+               	  	<li class="<?php echo ($converter == "speed-converter") ? "activecls" : "" ?>" id="speed-converter"><span><a href="<?php echo route_to('unit.convertor', 'speed-converter') ?>">Speed Converter </a> </span></li>
 
-               	  	<li id="angle-converter"><span><a href="<?php echo route_to('unit.convertor', 'angle-converter') ?>">Angle Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "angle-converter") ? "activecls" : "" ?>" id="angle-converter"><span><a href="<?php echo route_to('unit.convertor', 'angle-converter') ?>">Angle Converter </a> </span></li> 
 
-               	  	<li id="bytes-converter"><span><a href="<?php echo route_to('unit.convertor', 'bytes-converter') ?>">Bytes/Bits Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "bytes-converter") ? "activecls" : "" ?>" id="bytes-converter"><span><a href="<?php echo route_to('unit.convertor', 'bytes-converter') ?>">Bytes/Bits Converter </a> </span></li> 
 
-               	  	<li id="density-converter"><span><a href="<?php echo route_to('unit.convertor', 'density-converter') ?>">Density Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "density-converter") ? "activecls" : "" ?>" id="density-converter"><span><a href="<?php echo route_to('unit.convertor', 'density-converter') ?>">Density Converter </a> </span></li> 
 
-               	  	<li id="electric-current-converter"><span><a href="<?php echo route_to('unit.convertor', 'electric-current-converter') ?>">Electric Current Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "electric-current-converter") ? "activecls" : "" ?>" id="electric-current-converter"><span><a href="<?php echo route_to('unit.convertor', 'electric-current-converter') ?>">Electric Current Converter </a> </span></li> 
 
-               	  	<li id="energy-converter"><span><a href="<?php echo route_to('unit.convertor', 'energy-converter') ?>">Energy Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "energy-converter") ? "activecls" : "" ?>" id="energy-converter"><span><a href="<?php echo route_to('unit.convertor', 'energy-converter') ?>">Energy Converter </a> </span></li> 
 
-               	  	<li id="force-converter"><span><a href="<?php echo route_to('unit.convertor', 'force-converter') ?>">Force Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "force-converter") ? "activecls" : "" ?>" id="force-converter"><span><a href="<?php echo route_to('unit.convertor', 'force-converter') ?>">Force Converter </a> </span></li> 
 
-               	  	<li id="fuel-converter"><span><a href="<?php echo route_to('unit.convertor', 'fuel-converter') ?>">Fuel Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "fuel-converter") ? "activecls" : "" ?>" id="fuel-converter"><span><a href="<?php echo route_to('unit.convertor', 'fuel-converter') ?>">Fuel Converter </a> </span></li> 
 
-               	  	<li id="mass-converter"><span><a href="<?php echo route_to('unit.convertor', 'mass-converter') ?>">Mass Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "mass-converter") ? "activecls" : "" ?>" id="mass-converter"><span><a href="<?php echo route_to('unit.convertor', 'mass-converter') ?>">Mass Converter </a> </span></li> 
 
-               	  	<li id="power-converter"><span><a href="<?php echo route_to('unit.convertor', 'power-converter') ?>">Power Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "power-converter") ? "activecls" : "" ?>" id="power-converter"><span><a href="<?php echo route_to('unit.convertor', 'power-converter') ?>">Power Converter </a> </span></li> 
 
-               	  	<li id="pressure-converter"><span><a href="<?php echo route_to('unit.convertor', 'pressure-converter') ?>">Pressure Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "pressure-converter") ? "activecls" : "" ?>" id="pressure-converter"><span><a href="<?php echo route_to('unit.convertor', 'pressure-converter') ?>">Pressure Converter </a> </span></li> 
 
-               	  	<li id="time-converter"><span><a href="<?php echo route_to('unit.convertor', 'time-converter') ?>">Time Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "time-converter") ? "activecls" : "" ?>" id="time-converter"><span><a href="<?php echo route_to('unit.convertor', 'time-converter') ?>">Time Converter </a> </span></li> 
 
-               	  	<li id="astronomical-converter"><span><a href="<?php echo route_to('unit.convertor', 'astronomical-converter') ?>">Astronomical Converter </a> </span></li> 
+               	  	<li class="<?php echo ($converter == "astronomical-converter") ? "activecls" : "" ?>" id="astronomical-converter"><span><a href="<?php echo route_to('unit.convertor', 'astronomical-converter') ?>">Astronomical Converter </a> </span></li> 
 
-               	  	<li id="frequency-converter"><span><a href="<?php echo route_to('unit.convertor', 'frequency-converter') ?>">Frequency Converter </a> </span></li> </ul>
+               	  	<li class="<?php echo ($converter == "frequency-converter") ? "activecls" : "" ?>" id="frequency-converter"><span><a href="<?php echo route_to('unit.convertor', 'frequency-converter') ?>">Frequency Converter </a> </span></li> </ul>
 
                   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                    <!-- Ad 7 -->
