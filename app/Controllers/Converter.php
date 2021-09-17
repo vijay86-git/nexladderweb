b3113b080f49624b5f06c8f49a9d8edd95702287
Vijay Singh <vjmail17@gmail.com>
2021-09-17 21:28:58 +0530

    ...

--- app/Controllers/Converter.php
+++ app/Controllers/Converter.php
@@ -11,108 +11,200 @@ class Converter extends BaseController
             helper(['url', 'form', 'site']);
         }
 
+    public function converter()
+     {
+     	$prefix        = "Nexladder Web Tutorials -";
+     	$page_title    =  "$prefix Best online tool to provides all types of conversion units.";
+		$page_keywords =  "length converter, weight converter, volume converter, area converter, temperture converter, speed converter, angle converter, bytes/bits converter, density converter, electric current converter, energy converter, force converter, fuel converter, mass converter, power converter, pressure converter, time converter, astronmical converter, frequency converter"; 
+		$page_description = "Unit Conversion Tools - provides all types of conversion tools such as length converter, weight converter, volumen converter etc.";
+
+     	return view('Frontend/Converter/Converter', compact('page_title', 'page_keywords', 'page_description'));
+     }
+
 	public function unit($converter = '')
 	  {  
+	  	 $prefix = "Nexladder Web Tutorials -";
+
 	     switch($converter)
 	       {
 	       		case 'length-converter':
 	       			$units = LENGTH_CONVERTER;
+	       			$page_title    =  "$prefix Best Online tool to Convert Length and Supports all Units. i.e Km to Meter, Mile to centimeter ";
+	  	 			$page_keywords =  "length converter uk, length unit converter, length measurement converter, length convert, converter length, unit converter length, length converter table, convert length, length converter download, length convertion, focal length converter, free length converter, metric length converter, metric converter length, length units converter, unit length converter, length converters, diameter to length converter, length converter chart, human length converter"; 
+	  	 			$page_description = "Length Unit Conversion - converts meter, foot, inch, yard, kilometer, mile, nautical mile, light-year etc";
 	       			
 	       		break;
 
 	       		case 'weight-converter':
 	       			$units = WEIGHT_CONVERTER;
+	       			$page_title    =  "$prefix Best Weight Conversion Online tool to Convert Weight and Supports all Units. i.e kilogram to pound, pound to ounce";
+	  	 			$page_keywords =  "weight conversion, weight unit converter, unit converter weight, weight units converter, unit converter, units convert, convert weight units"; 
+	  	 			$page_description = "Weight and Mass Unit Conversion - converts kilogram, pound, gram, tonne, ton, ounce, newton etc";
 	       			
 	       		break;
 
 	       		case 'volume-converter':
 	       			$units = VOLUME_CONVERTER;
+	       			$page_title    =  "$prefix Best Online tool to Convert Volume and Supports all Units. i.e liter to gallon, pint to quart";
+	  	 			$page_keywords =  "volume conversion, conversion of volume, volume conversion calculator, conversion calculator volume, conversion volume, conversion for volume, volume conversions, conversion unit of volume, volume to volume conversion, volume unit conversion calculator, conversion units of volume, conversion in volume, conversion of units of volume, weight to volume conversion, volume to mass conversion calculator, length to volume conversion calculator, air volume conversion calculator, volume conversion calculator free, conversions volume";
+	  	 			$page_description =  "Volume Unit Conversion - converts liter, cubic meter, gallon, quart, pint, bushel, barrel etc";
 	       			
 	       		break;
 
 	       		case 'area-converter':
 	       			$units = AREA_CONVERTER;
-	       			
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Area and Supports all Units. i.e area to hectare, rood to homestead";
+	  	 			$page_keywords =  "converts square meter, square foot, square mile, acre, hectare";
+	  	 			$page_description =  "Area Unit Conversion - converts square meter, square foot, square mile, acre, hectare etc";
 	       		break;
 
 
 	       		case 'temperature-converter':
 	       			$units = TEMPERATURE_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Temperature and Supports all Units. i.e celsius to fahrenheit, fahrenheit to kelvin";
+	  	 			$page_keywords =  "temperature, temperture, celsius, celcious, fahrenheit, haranheit, kelvin";
+	  	 			$page_description =  "Convert temperatures to and from celsius, fahrenheit, and kelvin.";
+
 	       			
 	       		break;
 
 	       		case 'speed-converter':
 	       			$units = SPEED_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Speed and Supports all Units. i.e kilometers/hour to kilometers/second, miles/minute to miles/second";
+	  	 			$page_keywords =  "speed, kilometer/hour, kilometer/minute, mile/hour, feet/hour, yard, light year";
+	  	 			$page_description =  "Convert speed to and from kilometer, centimeter,feet per hours/minutes etc.";
+
 	       			
 	       		break;
 
 	       		case 'angle-converter':
 	       			$units = ANGLE_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Angle and Supports all Units. i.e degree to mil, mil to circle";
+	  	 			$page_keywords =  "degree, mil, circle, radian, sign, turn";
+	  	 			$page_description =  "Convert angle to and from degreen, mil, circle, turn etc.";
 	       			
 	       		break;
 
 	       		case 'bytes-converter':
 	       			$units = BYTES_CONVERTER;
-	       			
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Bytes/Bits and Supports all Units. i.e bits to bytes, megabytes to gigabytes, gigabytes to petabytes";
+	  	 			$page_keywords =  "bits, bytes, kilobytes, megabytes, gigabytes, petabytes";
+	  	 			$page_description =  "Convert bits/bytes to and from kb, mb, gb, tb, pd etc.";
+
+	       		
 	       		break;
 
 	       		case 'density-converter':
 	       			$units = DENSITY_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Density and Supports all Units. i.e converts to gram, gram to cubic, cubic to meter";
+	  	 			$page_keywords =  "converts, gram, per, cubic ,meter, kilogram/liter";
+	  	 			$page_description =  "Convert bits/bytes to and from kb,mb,gb,tb,pd etc.";
+
 	       			
 	       		break;
 
 	       		case 'electric-current-converter':
 	       			$units = ELECRIC_CURRENT_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Electric Current Converter and Supports all Units. i.e abampere to franklin, franklin to gilbert, gilbert to milliampere";
+	  	 			$page_keywords =  "converts,abampere, franklin,gilbert, milliampere, teraampere";
+	  	 			$page_description =  "Electric Current Conversion - converts abampere, franklin,gilbert, milliampere, teraampere...";	
 	       			
 	       		break;
 
 	       		case 'energy-converter':
 	       			$units = ENERGY_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Energy Converter and Supports all Units. i.e calories to Btu, Btu to gigajoules, gigajoules to joules";
+	  	 			$page_keywords =  "converts calories ,Btu ,gigajoules , joules ,therms";
+	  	 			$page_description =  "Energy Unit Conversion - converts calories ,Btu ,gigajoules , joules ,therms ...";	
 	       			
 	       		break;
 
 	       		case 'force-converter':
 	       			$units = FORCE_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Force Converter and Supports all Units. i.e dyne to newton, newton to pond, pond to gram-force";
+	  	 			$page_keywords =  "converts dyne, gram-force, newton, pond";
+	  	 			$page_description =  "Force Unit Conversion - converts dyne, gram-force, newton, pond...";	
 	       			
 	       		break;
 
 	       		case 'fuel-converter':
 	       			$units = FUEL_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Fuel Converter and Supports all Units. i.e gallons to miles, miles to liters, liters to kilometer";
+	  	 			$page_keywords =  "converts gallons ,miles, liters, kilometer";
+	  	 			$page_description =  "Fuel Unit Conversion - converts gallons ,miles, liters, kilometer...";
+
 	       			
 	       		break;
 
 	       		case 'mass-converter':
 	       			$units = MASS_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Mass Converter and Supports all Units. i.e carats to cental, cental to kilograms, kilograms to Earth masses";
+	  	 			$page_keywords =  "converts carats ,cental, Earth masses, kilograms";
+	  	 			$page_description =  "Mass Unit Conversion - converts carats ,cental, Earth masses, kilograms ...";
 	       			
 	       		break;
 
 	       		case 'power-converter':
 	       			$units = POWER_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Power Converter and Supports all Units. i.e watt to kilowatt, kilowatt to BTU per hour, BTU per hour to calories per second";
+	  	 			$page_keywords =  "converts watt, kilowatt, BTU per hour, calories per second, horsepower";
+	  	 			$page_description =  "Power Unit Conversion - converts watt, kilowatt, BTU per hour, calories per second, horsepower...";
+
 	       		break;
 
 	       		case 'pressure-converter':
 	       			$units = PRESSURE_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Pressure Converter and Supports all Units. i.e pascal to bar, bar to millibar, millibar to torr, torr to pounds per square inch";
+	  	 			$page_keywords =  "converts pascal, bar, millibar, torr, pounds per square inch";
+	  	 			$page_description =  "Pressure Unit Conversion - converts pascal, bar, millibar, torr, pounds per square inch...";
 	       			
 	       		break;
 
 	       		case 'time-converter':
 	       			$units = TIME_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Time Converter and Supports all Units. i.e second to minute, minute to hour, hour to day, day to tropical year";
+	  	 			$page_keywords =  "converts second, minute, hour, day, millisecond, tropical year";
+	  	 			$page_description =  "Time Unit Conversion - converts second, minute, hour, day, millisecond, tropical year...";
 	       			
 	       		break;
 
 	       		case 'astronomical-converter':
 	       			$units = ASTRONOMICAL_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Time Astronomical and Supports all Units. i.e astronmical unit to A.U., A.U. to au, au to light-year";
+	  	 			$page_keywords =  "astronmical unit, A.U., au, light-year, light years, parsec";
+	  	 			$page_description =  "Convert between several astronomical units.";
+
 	       			
 	       		break;
 
 	       		case 'frequency-converter':
 	       			$units = FREQUENCY_CONVERTER;
+
+	       			$page_title    =  "$prefix Best Online tool to Convert Time Astronomical and Supports all Units. i.e hertz to cycles, cycles to second, second to circular frequency, circular frequency to angular velocity, angular velocity to velocity, kilohertz to megahertz";
+	  	 			$page_keywords =  "frequency, hertz, cycles, second, circular frequency, angular velocity, revolutions, RPM, Hz, kilohertz, megahertz";
+	  	 			$page_description =  "Convert between many different frequency standards";
+
 	       			
 	       		break;
 	       }
 
 	     $js    = UNIT_CONVERTER_JS;
-		 return view('Frontend/Converter/Unit', compact('units', 'js', 'converter'));
+		 return view('Frontend/Converter/Unit', compact('units', 'js', 'converter', 'page_title', 'page_keywords', 'page_description'));
 	  }	
 
 }
---
+++ app/Views/Frontend/Converter/Converter.php
@@ -0,0 +1,129 @@
+<?php echo $this->extend('Frontend/Layouts/Template'); ?>
+<?php echo $this->section('content'); ?>
+
+<style>
+.converterBox{margin:10px;width:31%;border:1px dashed #ddd;border-radius:10px;line-height:80px;text-align:center;height:80px}
+.converterBox a{font-size:20px}
+</style>
+         <div class="">
+            <div class="row">
+               <div class="col-md-12">
+                  <!-- content -->
+                  <div class="row mrgnbtm15">
+                     <div class="col-md-12 mrgnbtm15">
+                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Ad 1 -->
+                        <ins class="adsbygoogle"
+                           style="display:block"
+                           data-ad-client="ca-pub-9716398444039739"
+                           data-ad-slot="5095344703"
+                           data-ad-format="auto"></ins>
+                        <script>
+                           (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                     </div>
+                  </div>
+
+                  <div class="row mrgnbtm35">
+                      <div class="col-md-12">
+
+                          <div class="col-md-3 converterBox">
+                             <a href="<?php echo route_to('unit.convertor', 'length-converter') ?>">Length Converter</a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'weight-converter') ?>">Weight Converter</a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'volume-converter') ?>">Volume Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'area-converter') ?>">Area Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'temperature-converter') ?>">Temperature Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                            <a href="<?php echo route_to('unit.convertor', 'speed-converter') ?>">Speed Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'angle-converter') ?>">Angle Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'bytes-converter') ?>">Bytes/Bits Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'density-converter') ?>">Density Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'electric-current-converter') ?>">Electric Current Converter </a> 
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'energy-converter') ?>">Energy Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'force-converter') ?>">Force Converter </a>
+                          </div>
+
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'fuel-converter') ?>">Fuel Converter </a>
+                          </div>
+
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'mass-converter') ?>">Mass Converter </a> 
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                           <a href="<?php echo route_to('unit.convertor', 'power-converter') ?>">Power Converter </a>
+                          </div>
+
+                          <div class="col-md-3 converterBox">
+                             <a href="<?php echo route_to('unit.convertor', 'pressure-converter') ?>">Pressure Converter </a>
+                          </div>
+                          <div class="col-md-3 converterBox">
+                             <a href="<?php echo route_to('unit.convertor', 'time-converter') ?>">Time Converter </a>
+                          </div>
+                          <div class="col-md-3 converterBox">
+                             <a href="<?php echo route_to('unit.convertor', 'astronomical-converter') ?>">Astronomical Converter </a>
+                          </div>
+                          <div class="col-md-3 converterBox">
+                             <a href="<?php echo route_to('unit.convertor', 'frequency-converter') ?>">Frequency Converter </a>
+                          </div>
+
+
+                      </div>
+                       
+                  </div>
+
+                  <div class="row mrgnTpBtm">
+                     <div class="col-md-12">
+                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- Ad 3 -->
+                        <ins class="adsbygoogle"
+                           style="display:inline-block;width:728px;height:90px"
+                           data-ad-client="ca-pub-9716398444039739"
+                           data-ad-slot="9117871264"></ins>
+                        <script>
+                           (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <?php $this->endSection(); ?>
