596a182b620c7924f8c48653b175c371a88c4be9
Vijay Singh <vjmail17@gmail.com>
2021-09-17 21:33:23 +0530

    ...

--- app/Views/Frontend/Partials/Footer.php
+++ app/Views/Frontend/Partials/Footer.php
@@ -10,7 +10,7 @@
                         <p>&copy; <?php echo date("Y"); ?> All rights reserved.</p>
                      </div>
                      <div class="col-md-6 pull-right" style="text-align:right">
-                        <a title="Home" href="<?php echo route_to('/'); ?>">Home</a>&nbsp;|&nbsp;<a title="About" href="<?php echo route_to('about'); ?>">About</a>&nbsp;|&nbsp;<a title="Disclaimer" href="<?php echo route_to('disclaimer'); ?>">Disclaimer</a>&nbsp;|&nbsp;<a title="Contact Us" href="<?php echo route_to('contact.get'); ?>">Contact Us</a>&nbsp;|&nbsp;<a title="Sitemap" href="<?php echo route_to('converter'); ?>">Converter</a>&nbsp;|&nbsp;<a title="Sitemap" href="<?php echo route_to('sitemap'); ?>">Sitemap</a>&nbsp;|&nbsp;<a title="Blog" href="<?php echo blogUrl(); ?>">Blog</a>
+                        <a title="Home" href="<?php echo route_to('/'); ?>">Home</a>&nbsp;|&nbsp;<a title="About" href="<?php echo route_to('about'); ?>">About</a>&nbsp;|&nbsp;<a title="Disclaimer" href="<?php echo route_to('disclaimer'); ?>">Disclaimer</a>&nbsp;|&nbsp;<a title="Contact Us" href="<?php echo route_to('contact.get'); ?>">Contact Us</a>&nbsp;|&nbsp;<a title="Converter" href="<?php echo route_to('converter'); ?>">Converter</a>&nbsp;|&nbsp;<a title="Sitemap" href="<?php echo route_to('sitemap'); ?>">Sitemap</a>&nbsp;|&nbsp;<a title="Blog" href="<?php echo blogUrl(); ?>">Blog</a>
                      </div>
                      <!--/ copyright -->
                   </div>
