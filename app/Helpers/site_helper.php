85073832f311f8012eed6e93376cb19025ee9557
Vijay Singh <vjmail17@gmail.com>
2021-11-30 22:02:01 +0530

    ...

--- app/Helpers/site_helper.php
+++ app/Helpers/site_helper.php
@@ -833,9 +833,11 @@ if ( ! function_exists("youtube_trends_api"))
             if($trends_res_qry->getNumRows()):
 
             	foreach ($trends_res_qry->getResult('array') as $res):
+            		if(! empty($res['title'])):
             		  $stats_data = json_decode($res['stats'], true);
             		  $thumbnails = json_decode($res['thumbnails'], true);
             	 	  $resp[] = ['id' => (int) $res['id'], 'title' => (string) $res['title'], 'yt_id' => (string) $res['yt_id'], 'thumbnails' => (string) $thumbnails['high']['url'], 'channel_title' => (string) $res['channel_title'], 'category_id' => (string) $res['category_id'], 'like' => (int) $stats_data['likeCount'], 'view' => (int) $stats_data['viewCount'], 'comment' => (int) $stats_data['commentCount'], 'dislike' => (int) $stats_data['dislikeCount'], 'favorite' => (int) $stats_data['favoriteCount'], 'published_at' => (string) $res['published_at']];
+            	 	endif;
             	endforeach;
 
           		$data_res['results'] = $resp;
@@ -913,9 +915,11 @@ if ( ! function_exists("youtube_trends_api"))
             if($trends_res_qry->getNumRows())
             	 {
             	 	 foreach ($trends_res_qry->getResult('array') as $res):
+            	 	 	if(! empty($res['title'])):
             	 	 	$stats_data = json_decode($res['stats'], true);
             	 	 	$thumbnails = json_decode($res['thumbnails'], true);
             	 	  $resp[] = ['id' => (int) $res['id'], 'title' => (string) $res['title'], 'yt_id' => (string) $res['yt_id'], 'thumbnails' => (string) $thumbnails['high']['url'], 'channel_title' => (string) $res['channel_title'], 'category_id' => (string) $res['category_id'], 'like' => (int) $stats_data['likeCount'], 'view' => (int) $stats_data['viewCount'], 'comment' => (int) $stats_data['commentCount'], 'dislike' => (int) $stats_data['dislikeCount'], 'favorite' => (int) $stats_data['favoriteCount'], 'published_at' => (string) $res['published_at']];
+            	 	  endif;
             	 	endforeach;
             	 }
             $data_res['results'] = $trends_res_qry->getResult();
