b14cc065f891978d36031fc9f1dbb6f3611f1b19
Vijay Singh <vjmail17@gmail.com>
2021-12-12 21:04:58 +0530

    ....

--- app/Views/Frontend/Pages/cron.php
+++ app/Views/Frontend/Pages/cron.php
@@ -13,20 +13,6 @@
 </style>
 
 		  <div class="bodyPart">
-            <div class="row">
-               <div class="col-md-12">
-                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                  <!-- Ad 1 -->
-                  <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-9716398444039739"
-                     data-ad-slot="5095344703"
-                     data-ad-format="auto"></ins>
-                  <script>
-                     (adsbygoogle = window.adsbygoogle || []).push({});
-                  </script>
-               </div>
-            </div>
 
             <div class="row">
                <div class="col-md-12 text-center mrgntp20">
@@ -54,6 +40,22 @@
 
                </div>
             </div>
+
+            <div class="row">
+               <div class="col-md-12">
+                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- Ad 1 -->
+                  <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-9716398444039739"
+                     data-ad-slot="5095344703"
+                     data-ad-format="auto"></ins>
+                  <script>
+                     (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
+               </div>
+            </div>
+            
          </div>
 
 <?php $this->endSection(); ?>
