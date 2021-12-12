8d415d0fe5090dd44ff4587e5e2160ef2634c161
Vijay Singh <vjmail17@gmail.com>
2021-12-12 20:54:45 +0530

    ...

--- app/Views/Frontend/Pages/cron.php
+++ app/Views/Frontend/Pages/cron.php
@@ -1,8 +1,8 @@
 <?php echo $this->extend('Frontend/Layouts/Template'); ?>
 <?php echo $this->section('content'); ?>
 <style>
-    #crontab {font-size:28px}
-    #crontab select { border:1px solid #ddd;padding:5px}
+    #crontab {font-size:24px}
+    #crontab select { border:1px solid #ddd;padding:3px}
     .gen{font-size:22px !important}
     .cron p {font-size:14px;margin:20px 0 0 0}
     #crontab_data { font-weight:bold;font-size:44px;color:#338700}
@@ -39,44 +39,21 @@
                     </div>
                   </div>
 
-                  
                </div>
-               <div class="col-md-1"></div>
             </div>
          </div>
 
 <?php $this->endSection(); ?>
-
 <?php echo $this->section('js'); ?>
 <script src="<?php echo loadAssetsFiles('build/assets/js/cron.js?v=1.0'); ?>"></script>
-
-
-   <script type="text/javascript">
-    $(document).ready(function() {
-        $('#crontab').cron({
-            initial: "42 3 * * 5",
-            onChange: function() {
-                $('#crontab_data').text($(this).cron("value"));
-            }
-        });
-
-        /*$('#example1b').cron({
-            initial: "42 3 * * 5",
-            onChange: function() {
-                $('#example1b-val').text($(this).cron("value"));
-            },
-            useGentleSelect: true
-        });
-        $('#example2').cron({
-            initial: "42 3 * * 5",
-            effectOpts: {
-                openEffect: "fade",
-                openSpeed: "slow"
-            },
-            useGentleSelect: true
-        });
-        
-        */
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#crontab').cron({
+        initial: "42 3 * * 5",
+        onChange: function() {
+            $('#crontab_data').text($(this).cron("value"));
+        }
     });
-    </script>
+});
+</script>
 <?php $this->endSection(); ?>
