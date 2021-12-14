e6d7cf65a183d5e38e4ad828db7cee471877f96b
Vijay Singh <vjmail17@gmail.com>
2021-12-14 22:53:12 +0530

    ...

--- app/Views/Frontend/Pages/what_is_my_ip.php
+++ app/Views/Frontend/Pages/what_is_my_ip.php
@@ -93,7 +93,7 @@
                </div>
 
                <div class="col-md-6 text-center">
-                  <h1><i class="fa fa-hand-o-down" aria-hidden="true"></i> Location On Map</h1>
+                  <h1 style="background:#f9f9f9;margin-bottom:0;line-height:60px;"><i class="fa fa-hand-o-down" aria-hidden="true"></i> Location On Map</h1>
                   <iframe width="100%" height="525" id="" src="https://maps.google.com/maps?q=<?php echo $response['geoplugin_latitude'] ?>,<?php echo $response['geoplugin_longitude'] ?>&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 
                </div>
